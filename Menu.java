448ef54de9febb5815739e272445240f94778959
Erick Barrera <dannbj1@gmail.com>
2023-07-17 12:15:25 -0600

    New commit

---
+++ Menu.java
@@ -0,0 +1,19 @@
+/**
+ * Write a description of class Menu here.
+ * 
+ * @author Erick 
+ * @version 1.0.0
+ */
+public class Menu  
+{
+    /**
+     * Constructor for objects of class Menu
+     */
+    public Menu()
+    {
+    }
+    public String Boton(){
+        return "Modo Historia";
+    }
+    
+}
